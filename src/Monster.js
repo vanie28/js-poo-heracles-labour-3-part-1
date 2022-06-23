a00d06a690ee9af8e74e29e84d9f670d5fedebe8
vanie28 <sylvanie.giraudt@sncf.fr>
2022-06-23 14:51:33 +0200

    firstcommit

---
+++ src/Monster.js
@@ -0,0 +1,8 @@
+class Monster extends Fighter {
+    constructor(name, strength, dexterity, image) {
+      this.name = name;
+      this.strength = strength;
+      this.dexterity = dexterity;
+      this.image = image;
+    }
+}
